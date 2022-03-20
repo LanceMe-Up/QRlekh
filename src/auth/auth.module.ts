fb2507e13a4416920bfdac92a53cf83799ab5e9c
kishorkc <kishor8007kc@gmail.com>
2022-03-20 19:51:53 +0545

    add heroku

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
 import { PassportModule } from '@nestjs/passport';
-import { OtpService } from '../otp/otp.service';
-import { PrismaService } from '../prisma.service';
 import { UserModule } from '../user/user.module';
-import { UserService } from '../user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -20,15 +17,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    LocalStrategy,
-    JwtStrategy,
-    PrismaService,
-    UserService,
-    JwtService,
-    OtpService,
-  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}

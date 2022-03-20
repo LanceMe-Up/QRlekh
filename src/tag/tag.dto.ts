f8891ccc4e71addb8ae53dc816ec99ff23ffcc50
kishorkc <kishor8007kc@gmail.com>
2022-03-20 20:16:47 +0545

    swagger api tag add

--- src/tag/tag.dto.ts
+++ src/tag/tag.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class TagDto {
+  @ApiProperty({ example: 'Temple', description: 'Tag For' })
   @IsString()
   tagName: string;
 
+  @ApiProperty({ example: '12.058755', description: 'latitude number' })
   @IsNumber()
   lat: number;
 
+  @ApiProperty({ example: '26.058755', description: 'longitude number' })
   @IsNumber()
   long: number;
 }
